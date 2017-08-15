88165f8fc480473eef336d3ea09cbbf0cba164fb
Duc Dao <ducdh1@smartosc.com>
2017-08-15 09:15:16 +0700

    Add task runner

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'scss',
+                    src: ['styles.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify: {
+            build: {
+                src: ['js/*.js'],
+                dest: 'js/app.min.js'
+            }
+        },
+        watch: {
+            css: {
+                files: 'scss/styles.scss',
+                tasks: ['sass', 'cssmin']
+            },
+            js: {
+                files: 'js/*.js',
+                tasks: ['uglify']
+            },
+            options: {
+                reload: true
+            }
+        }
+    });
+
+    // Load Grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Register Grunt tasks
+    grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
+    grunt.registerTask('dev', ['watch']);
+};
